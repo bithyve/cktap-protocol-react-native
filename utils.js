b8febeb3411e7036e7a717de7118c650dea9e2f7
PraneethGunas <praneethgunas404@gmail.com>
2022-04-14 00:14:17 +0530

    added utils

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+import { CARD_NONCE_SIZE, USER_NONCE_SIZE } from './constants';
+
 // xor_bytes
 // pick_nonce
 // path2str
@@ -7,11 +9,28 @@
 // recover_pubkey
 // recover_address
 // force_bytes
-// render_address
-// verify_master_pubkey
-// verify_derive_address
 import { bech32 } from 'bech32';
 import { hash160 } from './compat';
+
+function verify_master_pubkey(pub, sig, chain_code, my_nonce, card_nonce) {
+  // using signature response from 'deriv' command, recover the master pubkey
+  // for this slot
+  // TODO: verify if b'string' has some sp meaning in python
+  const msg = 'OPENDIME' + card_nonce + my_nonce + chain_code;
+
+  if (msg.length !== 8 + CARD_NONCE_SIZE + USER_NONCE_SIZE + 32) {
+    console.warn('verify_master_pubkey: invalid message length');
+    return;
+  }
+
+  const ok = CT_sig_verify(pub, sha256s(msg), sig);
+  if (!ok) {
+    console.warn('verify_master_pubkey: bad sig in verify_master_pubkey');
+    return;
+  }
+
+  return pub;
+}
 
 function render_address(pubkey, testnet = false) {
   // make the text string used as a payment address
@@ -34,4 +53,4 @@ function verify_derive_address(chain_code, master_pub, testnet = false) {
   return render_address(pubkey, (testnet = testnet)), pubkey;
 }
 
-module.exports = { verify_derive_address };
+module.exports = { verify_derive_address, verify_master_pubkey };
